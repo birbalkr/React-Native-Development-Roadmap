14855e14f0ac12787d1b78f13fe170e6d923a283
birbalkr <birbalkr1435@gmail.com>
2025-06-10 23:42:13 +0530

    Flexbox for layout

--- react_app/app/index.tsx
+++ react_app/app/index.tsx
@@ -3,7 +3,15 @@ import '.././global.css';
 
 export default function Index() {
   return (
-    <></>
+    <View style={{ flex: 1 }}>
+
+      <View className='bg-pink-500 flex-1'></View>
+      <View style={{backgroundColor:'green', flex: 1,}}></View>
+      <View style={{backgroundColor:'blue', flex: 1,}}></View>
+      <View style={{backgroundColor:'purple', flex: 1,}}></View>
+      <View style={{backgroundColor:'orange', flex: 1,}}></View>
+      
+    </View>
   );
 }
 
