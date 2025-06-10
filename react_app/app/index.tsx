750ecbf56d58ee6fbf2f344409fc15470bf51d34
birbalkr <birbalkr1435@gmail.com>
2025-06-11 01:00:36 +0530

    Platform-specific code (Platform.OS)

--- react_app/app/index.tsx
+++ react_app/app/index.tsx
@@ -1,17 +1,29 @@
-import { View } from "react-native";
-import '.././global.css';
+import { Platform, View, Text } from 'react-native';
+import '.././global.css'; // assuming Tailwind setup
 
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
 
-      <View className='bg-pink-500 flex-1'></View>
-      <View style={{backgroundColor:'green', flex: 1,}}></View>
-      <View style={{backgroundColor:'blue', flex: 1,}}></View>
-      <View style={{backgroundColor:'purple', flex: 1,}}></View>
-      <View style={{backgroundColor:'orange', flex: 1,}}></View>
-      
+      {Platform.OS === 'ios' && (
+        <View className='bg-pink-500 flex-1'>
+          <View style={{ backgroundColor: 'green', flex: 2 }} />
+          <View style={{ backgroundColor: 'blue', flex: 5 }} />
+          <View style={{ backgroundColor: 'purple', flex: 1 }} />
+          <View style={{ backgroundColor: 'orange', flex: 3 }} />
+        </View>
+      )}
+
+      {Platform.OS === 'android' && (
+        <View style={{ backgroundColor: 'red', flex: 1 }}>
+          <View style={{ backgroundColor: 'green', flex: 1 }} />
+          <View style={{ backgroundColor: 'blue', flex: 1 }} />
+          <View style={{ backgroundColor: 'purple', flex: 1 }} />
+          <View style={{ backgroundColor: 'orange', flex: 1 }} />
+        </View>
+      )}
+
+
     </View>
   );
 }
-
