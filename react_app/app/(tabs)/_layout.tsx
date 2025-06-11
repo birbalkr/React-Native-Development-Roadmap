083c18d7920cf0bd654423aa0bf50c766ecd12a4
birbalkr <birbalkr1435@gmail.com>
2025-06-11 11:00:48 +0530

    Tab navigate update some code

--- react_app/app/(tabs)/_layout.tsx
+++ react_app/app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="index"
                 options={{
+                    headerShown: false,
                     title: 'Home',
                     tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
                 }}
@@ -14,6 +15,7 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="settings"
                 options={{
+                    headerShown: false,
                     title: 'Settings',
                     tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
                 }}
