d4f804b93677100d98323afaa4856b58de5f1732
birbalkr <birbalkr1435@gmail.com>
2025-06-11 10:57:14 +0530

    Tab navigate

--- react_app/app/_layout.tsx
+++ react_app/app/_layout.tsx
@@ -1,24 +1,10 @@
-// import { Stack } from "expo-router";
 
-// export default function RootLayout() {
-//   return <Stack />;
-// }
 import { Stack } from 'expo-router';
 
-export default function Layout() {
+export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      {/* Optionally configure static options outside the route.*/}
-      <Stack.Screen name="home" options={{}} />
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
